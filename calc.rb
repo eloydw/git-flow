2af34b92e4dee3d0a4285adbe0120b51590e56e3
Eloy GL <eloydw@gmail.com>
2015-02-09 13:16:31 +0100

    Implementada la función suma

--- calc.rb
+++ calc.rb
@@ -3,4 +3,20 @@ class Calc
     @a = a
     @b = b
   end
+
+  def sum
+    @a + @b
+  end
+
+  def subtraction
+    @a - @b
+  end
+
+  def multiplication
+    @a * @b
+  end
+
+  def division
+    @a / @b unless @b == 0
+  end
 end
